bb7684ddd641725813d480b8cd27d2b8283f8832
akashsainics21m003 <98253868+akashsainics21m003@users.noreply.github.com>
2022-04-01 09:30:39 +0530

    Update question2.py

--- assignment2/partA/question2.py
+++ assignment2/partA/question2.py
@@ -64,7 +64,7 @@ def do_hyperparameter_search_using_wandb():
       "batch_norm": {"values": [True, False]},
       "drop_out": {"values": [0.2, 0.3, 0.4]},
       "padding": {"values": ['same', 'valid']},
-      "dense_size": {"values": [64, 128]},
+      "dense_size": {"values": [32,64, 128]},
       "filter_org": {"values": [1, 2, 0.5]},
       "input_size": {"values": [INPUT_SIZE]},
       "output_size": {"values": [OUTPUT_SIZE]}
